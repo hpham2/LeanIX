75ded404ca60365bc40b0c5296f32e469a6fedf6
hai pham <phamhoangtrunghai@gmail.com>
2019-08-10 14:33:54 +0200

    add table

--- LeanIX/src/app/app.module.ts
+++ LeanIX/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+
+import { TableComponent } from './table/table.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
